7388aa35278570eac331736cc74bdf25410d3ff3
Vojtěch Javůrek <vojta.javurek@gmail.com>
2026-05-23 08:13:55 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Welcome! to Calculator\n";
+    std::cout << "\n";
+    std::cout << "Choose one operation\n";
+    std::cout << "1. Add\n";
+    std::cout << "2. Subtract\n";
+    std::cout << "2. Exit\n";
+
+
+
+}
